a1f72cbfa41d59b95f331196bd5843dff07aee81
tlamer <tlamer@gmail.com>
2017-12-04 15:05:05 +0100

    Improve logging.

--- automail.py
+++ automail.py
@@ -243,10 +243,11 @@ def main():
         return
 
     missing_vars = tmpl_vars - set(args.jinja_vars.keys())
+    LOGGER.debug("Missing variables: %s", missing_vars)
 
     if args.noninteractive:
         if missing_vars:
-            LOGGER.error("Missing jinja variables in batch mode: %s",
+            LOGGER.error("Missing jinja variables in noninteractive mode: %s",
                          missing_vars)
             return 1
         message = tmpl.render(args.jinja_vars)
